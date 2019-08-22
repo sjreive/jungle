5ab761f8174551618a2cf5fbc47b6b6f47a6f74a
Sarah Reive <sarahjreive@gmail.com>
2019-08-22 13:48:31 -0400

    Implemented user auth for login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,16 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
 
 
 
